2096680cf8a72d5b9d5392835df00ca384e027cb
kazmierczyk.m@gmail.com <kazmierczyk.m@gmail.com>
2020-04-11 00:39:51 +0200

    com

---
+++ multipletargetapp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace multipletargetapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
